2bd3cd692d20b9965066b1e8e5119eedf6d8e977
nmh14022005 <152689824+NMH2005@users.noreply.github.com>
2026-07-18 01:21:03 +0700

    end game

--- Assets/Scripts/UI/EndGameUI.cs
+++ Assets/Scripts/UI/EndGameUI.cs
@@ -1,16 +1,51 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class EndGameUI : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+public class EndGameUI : MonoBehaviour {
+    [SerializeField] private GameObject panel;
+    [SerializeField] private TMP_Text titleText;
+
+    private void OnEnable()
     {
-        
+        EventManager.OnGameOver += ShowGameOver;
+        EventManager.OnAllWavesCompleted += ShowVictory;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        EventManager.OnGameOver -= ShowGameOver;
+        EventManager.OnAllWavesCompleted -= ShowVictory;
+    }
+
+    private void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    private void ShowGameOver()
+    {
+        titleText.text = "Game Over";
+        panel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void ShowVictory()
+    {
+        titleText.text = "Victory!";
+        panel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScene");
     }
 }
